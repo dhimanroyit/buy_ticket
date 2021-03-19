555ad082f2fef9940259d21f87524a9954514ab7
dhimanroyit <dhimanroyict@gmail.com>
2021-03-19 20:17:11 +0600

    create folder structure

---
+++ src/container/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
